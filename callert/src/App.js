84941707d8cdc26d2dd19a44ebfe7d3b33584755
Marc-Antoine-Lune <luneschima@gmail.com>
2020-10-30 19:17:02 +0100

    add routing - add component home, profile and blog

--- callert/src/App.js
+++ callert/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import SignIn from './Components/SignIn'
+import Profile from './Components/Profile'
+import Home from './Components/Home'
+import Blog from './Components/Blog'
 
 
 function App() {
 
   return (
     <div className="App">
-      <SignIn/>
+      <Switch>
+        <Route path="/" exact component={SignIn} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/home" component={Home} />
+        <Route path="/blog" component={Blog} />
+      </Switch>
     </div>
   );
 }
