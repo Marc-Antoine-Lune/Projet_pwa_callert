af48f99b6d2e4309b9d1c8fdb808047aa0b80ab5
Saveriu Battesti <saveriubattesti@gmail.com>
2020-10-31 23:55:58 +0100

    get user data from api

--- callert/src/Components/Profile.js
+++ callert/src/Components/Profile.js
@@ -1,37 +1,64 @@
 import '../App.css';
-import React from 'react';
+import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Loading from 'react-loading-spinkit';
 
+const API = 'https://us-central1-callert-b38f5.cloudfunctions.net/webApi/api/v1/users/';
+// TODO : id dynamique
+const ID = '2';
 
-export default function Profile() {
-  const props = {
-    lastName: 'Battesti',
-    firstName: 'Saveriu',
-    mail: 'saveriubattesti@gmail.com',
-    adress: 'Résidence Les Collines du Ricanto',
-    zipCode: '20090',
-  };
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+  }
 
-  const tfStyle = {
-    width: "90%",
-    marginTop: "20px",
-  };
+  componentDidMount() {
+    fetch(API + ID)
+      .then(response => response.json())
+      .then(result => this.setState({ user: result.data }));
+  }
 
-  const profileContainerStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  };
+  render() {
+    const tfStyle = {
+      width: "90%",
+      marginTop: "20px",
+    };
 
-  return (
-    <div style={profileContainerStyle}>
-      <TextField style={tfStyle} variant="outlined" id="standard-full-width" color="secondary" label='Last Name' defaultValue={props.lastName} />
-      <TextField style={tfStyle} variant="outlined" id="standard-full-width" color="secondary" label='First Name' defaultValue={props.firstName} />
-      <TextField style={tfStyle} variant="outlined" id="standard-full-width" color="secondary" label='E-mail' defaultValue={props.mail} />
-      <TextField style={tfStyle} variant="outlined" id="standard-full-width" color="secondary" label='Adress' defaultValue={props.adress} />
-      <TextField style={tfStyle} variant="outlined" id="standard-full-width" color="secondary" label='Zip Code' defaultValue={props.zipCode} />
-      <Button style={{ marginTop: "20px" }} variant="contained" color="secondary">Save</Button>
-    </div>
-  );
+    const profileContainerStyle = {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    };
+
+    const loadingStyle = {
+      color: 'red',
+    }
+
+    const user = this.state.user;
+
+    if (user === null) {
+      return (
+        <div style={{ height: '100vh', width: '100vw' }}>
+          <Loading show={true} name='circle' color='#F50357' />
+        </div>
+      );
+    }
+
+    return (
+      <div style={profileContainerStyle}>
+        <TextField style={tfStyle} variant="outlined" id="standard-full-width" color="secondary" label='Last Name' defaultValue={user.name} />
+        <TextField style={tfStyle} variant="outlined" id="standard-full-width" color="secondary" label='First Name' defaultValue={user.firstName} />
+        <TextField style={tfStyle} variant="outlined" id="standard-full-width" color="secondary" label='E-email' defaultValue={user.email} />
+        <TextField style={tfStyle} variant="outlined" id="standard-full-width" color="secondary" label='Adress' />
+        <TextField style={tfStyle} variant="outlined" id="standard-full-width" color="secondary" label='Zip Code' defaultValue={user.contactNumber} />
+        <Button style={{ marginTop: "20px" }} variant="contained" color="secondary">Save</Button>
+      </div>
+    );
+  }
 }
+
+export default Profile;
